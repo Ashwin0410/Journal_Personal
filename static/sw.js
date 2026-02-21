3af754aa37e3b2a740c41a15373ca18f37dfc84b
Ashwin0410 <programmingashwin@gmail.com>
2026-02-21 12:36:28 +0530

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'journal-v1';
+const ASSETS = [
+  '/',
+  '/static/index.html',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;0,700;1,400;1,500&family=Nunito:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
